293920bd3ea25b329da5778f9dbf6b7c13376df1
skyba <ksv@evogence.com>
2018-04-01 23:22:52 +0300

    control flow statement: Exercises

--- src/main/java/docs/SwitchStatement.java
+++ src/main/java/docs/SwitchStatement.java
@@ -1,5 +1,7 @@
 package docs;
 
+import java.util.ArrayList;
+
 public class SwitchStatement {
     public static void main(String[] args) {
         int number = 1;
@@ -57,7 +59,7 @@ public class SwitchStatement {
         }
         {
             int count = 0;
-            while (count < 10){
+            while (count < 10) {
                 count++;
                 System.out.println("count: " + count);
             }
@@ -67,18 +69,128 @@ public class SwitchStatement {
             do {
                 System.out.println("count1 is: " + count1);
                 count1++;
-            }while (count1 < 10);
+            } while (count1 < 10);
         }
         {
-            for (int i = 1; i < 10; i++){
+            for (int i = 1; i < 10; i++) {
                 System.out.println("for = " + i);
             }
         }
         {
-            int[]numbers = {1,2,3,4,5,6,7,8,9};
-            for (int item:numbers){
+            int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            for (int item : numbers) {
                 System.out.println("item is: " + item);
             }
+        }
+        {
+            ArrayList<String> names = new ArrayList<>();
+            names.add("Jack");
+            names.add("Anna");
+            names.add("Jon");
+            names.add("Arnold");
+            for (String name : names) {
+                System.out.println("Name: " + name);
+            }
+        }
+        {
+            int[] arrayOfDigits = {4, 3, 70, 15,
+                    101, 1, 34, 56, 44, 777, 0};
+            int searchFor = 777;
+            int i;
+            boolean foundIt = false;
+            for (i = 0; i < arrayOfDigits.length; i++) {
+                if (arrayOfDigits[i] == searchFor) {
+                    foundIt = true;
+                    break;
+                }
+            }
+            if (foundIt) {
+                System.out.println("Found " + searchFor + " at index " + i);
+            } else {
+                System.out.println(searchFor + " not in the array");
+            }
+        }
+        {
+            //labeled "search" for break
+            int[][] arrayOfInts = {
+                    {1, 3, 5, 7, 9},
+                    {2, 4, 6, 8, 10},
+                    {0, 11, 48, 777}
+            };
+            int searchIt = 48;
+
+            int i;
+            int j = 0;
+            boolean foundInt = false;
+
+            search:
+            for (i = 0; i < arrayOfInts.length; i++) {
+                for (j = 0; j < arrayOfInts[i].length; j++) {
+                    if (arrayOfInts[i][j] == searchIt) {
+                        foundInt = true;
+                        break search;
+                    }
+                }
+            }
+            if (foundInt) {
+                System.out.println("found " + searchIt + " at " + i + ", " + j);
+            } else {
+                System.out.println(searchIt + " is absent in this array");
+            }
+        }
+        {
+            //continue Statement
+            String searchMe = "my own text " + "for test t's chars";
+            int max = searchMe.length();
+            int numTs = 0;
+
+            for (int i = 0; i < max; i++) {
+                if (searchMe.charAt(i) != 't') {
+                    continue;
+                }
+                numTs++;
+            }
+            System.out.println("found " + numTs + " t's in the string");
+        }
+        {
+            //DO WHILE STATEMENT evaluates its expression at the bottom of the loop instead of the top
+            int num = 1;
+            do {
+                System.out.println("result = " + num);
+                num++;
+            } while (num < 5);
+        }
+        {
+            //infinite loop using the for statement
+//            for (int i=3; i>2; i++){
+//                System.out.println(i);
+//            }
+        }
+        {
+            // infinite loop using the while statement
+//            int count = 5;
+//            while (count > 4){
+//                System.out.println(count);
+//            }
+        }
+        {
+            int sum = -3;
+            if (sum >= 0)
+                if (sum == 0)
+                    System.out.println("first string");
+                else System.out.println("second string");
+
+            System.out.println("third string");
+        }
+        {
+            int sum = -3;
+            if (sum >= 0) {
+                if (sum == 0) {
+                    System.out.println("first string");
+                } else System.out.println("second string");
+            }
+                System.out.println("third string");
+
         }
     }
 }
