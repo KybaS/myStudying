d65d2ecb7a4b90cb755392a9564f85be9fb4cbad
KybaS <sergeykiba2012@gmail.com>
2018-04-04 15:21:23 +0300

    Passing Reference Data Type Arguments

--- src/main/java/docs/classes_and_objects/MyMethods.java
+++ src/main/java/docs/classes_and_objects/MyMethods.java
@@ -6,4 +6,14 @@ public class MyMethods {
         int x = a * b * c;
         System.out.println(x);
     }
+
+    void myMethod(Triangle triangle, int a, int b, int c) {
+        triangle.setA(triangle.getA() + a);
+        triangle.setB(triangle.getB() + b);
+        triangle.setC(triangle.getC() + c);
+        triangle = new Triangle(0, 0, 0);
+
+        int x = a * b * c;
+        System.out.println(x + "" + triangle);
+    }
 }
--- src/main/java/docs/classes_and_objects/MyObjects.java
+++ src/main/java/docs/classes_and_objects/MyObjects.java
@@ -1,9 +1,14 @@
 package docs.classes_and_objects;
 
 public class MyObjects {
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
         MyMethods methods = new MyMethods();
+        Triangle myTriangle = new Triangle();
+
         methods.areaOfTriangle(10, 10, 10);
+
+        methods.myMethod(myTriangle, 5, 5, 5);
+
     }
 }
